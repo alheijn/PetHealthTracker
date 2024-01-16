b57728e005aa4a288d808b4b37828093dda15f73
Patrizia Neubauer <105806213+patrizianeubauer@users.noreply.github.com>
2024-01-16 18:07:07 +0100

    Update ContentView.swift

--- PetHealthTracker/ContentView.swift
+++ PetHealthTracker/ContentView.swift
@@ -9,80 +9,85 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @State var startTrackingIsClicked: Bool = false
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        ZStack {
+            Color.secondBackground
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(alignment: .center, spacing: 10) {
+                
+                VStack(alignment: .center, spacing: 10){
+                        Text("Pet Health Tracker")
+                        .font(.system(size: 35, weight: .bold))
+                        .padding(10)
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 0)
+                                    .stroke(Color.white, lineWidth: 2)
+                                    .background(Color.white.opacity(0.2))
+                        )
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                .foregroundColor(.white)
+                .padding(.top, 50)
+                
+                
+                Image("startAppPicture")
+                    .resizable()  // Make the image resizable
+                    .scaledToFit() // Maintain the image's aspect ratio while fitting it within the frame
+                    .frame(maxWidth: .infinity, maxHeight: .infinity) // Vollständige Breite des verfügbaren Bereichs
+                    .frame(width: UIScreen.main.bounds.width * 0.7) // 70% der Bildschirmbreite
+                    .overlay(Circle()
+                        .stroke(Color.primaryBackground, lineWidth: 8)
+                        .frame(width: UIScreen.main.bounds.width * 0.8) // Anpassen der Größe des Kreises
+                        .background(
+                            Circle().fill(Color.white.opacity(0.3)) // Ändern Sie die Hintergrundfarbe hier, z.B. in Rot
+                        )
+                    )
+
+                VStack(alignment: .center, spacing: 10){
+                        Text("Keep track of your pet's health records, appointments, etc.")
+                        .font(.system(size: 25, weight: .regular))
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                .foregroundColor(.white)
+                .padding(.bottom, 20)
+                
+                Spacer()
+                
+                Button(action: {
+                    startTrackingIsClicked.toggle()
+                }) {
+                    Text("Start tracking")
+                        .foregroundColor(.black)
+                        .font(.system(size: 18, weight: .bold))
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                .fullScreenCover(isPresented: $startTrackingIsClicked, content: {
+                    MainView()
+                })
+                .frame(width: 150, height: 50, alignment: .center)
+                .background(Color.primaryBackground)
+                .clipShape(RoundedCorner(radius: 35, corners: [.topLeft, .bottomLeft, .bottomRight, .topRight]))
+                .padding(.bottom, 40)
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct RoundedCorner: Shape {
+    var radius: CGFloat = 0.0
+    var corners: UIRectCorner = []
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: .init(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
